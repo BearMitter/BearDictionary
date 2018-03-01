909fb54d0164fe302d4e163cbbd3411119e83d6f
Bear Mitter <bear19950611@gmail.com>
2018-03-01 15:22:07 +0800

    df

--- src/main/java/remember_words/WordsRemember.java
+++ src/main/java/remember_words/WordsRemember.java
@@ -8,7 +8,7 @@ import java.net.URL;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
+import com.google.gson.JsonArray; 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
